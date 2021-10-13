7ada23bbb883760f82b1fdea83bbe34a6711e284
Jorge Menendez Hurtado <jorge.menendez@rimac.com.pe>
2021-10-12 20:24:49 -0500

    primera version

---
+++ index.js
@@ -0,0 +1,39 @@
+const express  = require('express')
+const cors     = require('cors');
+var mysql      = require('mysql');
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    port     : '3306',
+    user     : 'xpertpro_jorge',
+    password : 'bootcamp_aquainnova'
+  });
+
+connection.connect();
+
+const app = express();
+app.use(express.json())
+app.use(cors({
+    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+}));
+
+app.get('/', (req,res)=>{res.send("Servidor Iniciado")})
+
+app.post('/insert',async (req,res)=>{
+    let rsp="OK";
+
+    await connection.query(`
+        INSERT INTO bootcamp.cliente (nombres,apellidos,correo,telefono,empresa,cargo,pais,region)
+        VALUES ('${req.body.nombres}','${req.body.apellidos}','${req.body.correo}','${req.body.telefono}',
+        '${req.body.empresa}','${req.body.cargo}','${req.body.pais}','${req.body.region}');
+    `, function(err, rows, fields) {
+        if (err)rsp=err;
+        //console.log('The solution is: ', rows[0].solution);
+      });
+      connection.end();
+     res.send(rsp)
+})
+
+app.listen(3030,()=>{
+    console.log('servidor iniciado');
+})
